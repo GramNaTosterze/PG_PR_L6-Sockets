c1a427625de2acc31a1cf0207293971ddef4d45f
Krzysztof Napiórkowski <krzysna@gmail.com>
2023-04-21 12:26:23 +0200

    Dodaj pliki projektów.

---
+++ PG_PR_L6-Sockets/Program.cs
@@ -0,0 +1,116 @@
+﻿using PG_PR_L6_Sockets;
+using System;
+using System.Data;
+using System.Diagnostics;
+using System.IO.Pipes;
+using System.Net;
+using System.Net.Sockets;
+using System.Reflection;
+using System.Text;
+using System.Xml.Linq;
+
+class IPC
+{
+    const string pipeName = "PG_PR_L6-Sockets";
+    const string resName = "Sock-RES";
+    static IPHostEntry host = Dns.GetHostEntry("localhost");
+    static IPAddress ipAddress = host.AddressList[0];
+    static IPEndPoint localEP = new IPEndPoint(ipAddress, 11000);
+    static IPEndPoint responseEP = new IPEndPoint(ipAddress, 11100);
+    static void Main(string[] args)
+    {
+        if (args.Length == 0)
+            Server();
+        else
+            Client(args);
+
+    }
+    static async void Server()
+    {
+        try {
+
+            // Create a Socket that will use Tcp protocol
+            Socket listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            Socket responseListener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            // A Socket must be associated with an endpoint using the Bind method
+            listener.Bind(localEP);
+            responseListener.Bind(responseEP);
+
+            listener.Listen(10);
+            responseListener.Listen(10);
+
+            Console.WriteLine("Waiting for a connection...");
+            
+
+            string clientPath = @"PG_PR_L6-Sockets.exe";
+
+            var startInfo = new ProcessStartInfo(clientPath, pipeName + " " + resName);
+            startInfo.UseShellExecute = true;
+            Process childProcess = Process.Start(startInfo);
+
+            Socket handler = listener.Accept();
+            Socket responseHandler = responseListener.Accept(); 
+
+            new UniqueList(handler, responseHandler);
+            
+            handler.Shutdown(SocketShutdown.Both);
+            handler.Close();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+
+        Console.WriteLine("\n Press any key to continue...");
+        Console.ReadKey();
+    }
+    public static void Client(string[] args)
+    {
+        byte[] bytes = new byte[1024];
+
+        try
+        {
+
+            // Create a TCP/IP  socket.
+            Socket sender = new Socket(ipAddress.AddressFamily,
+                SocketType.Stream, ProtocolType.Tcp);
+            Socket responce = new Socket(ipAddress.AddressFamily,
+                SocketType.Stream, ProtocolType.Tcp);
+
+
+            // Connect the socket to the remote endpoint. Catch any errors.
+            try
+            {
+                // Connect to Remote EndPoint
+                sender.Connect(localEP);
+                responce.Connect(responseEP);
+
+                new UniqueList(sender, responce);
+
+                // Release the socket.
+                sender.Shutdown(SocketShutdown.Both);
+                sender.Close();
+
+            }
+            catch (ArgumentNullException ane)
+            {
+                Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+            }
+            catch (SocketException se)
+            {
+                Console.WriteLine("SocketException : {0}", se.ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unexpected exception : {0}", e.ToString());
+            }
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+
+        Console.Read();
+    }
+}
---
+++ PG_PR_L6-Sockets/UniqueList.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO.Pipes;
+using System.Net.Sockets;
+using System.Text;
+using static IPC;
+
+namespace PG_PR_L6_Sockets
+{
+    internal class UniqueList
+    {
+        List<int> numbers = new List<int>();
+        Thread read;
+        Thread write;
+        public enum MessageType
+        {
+            Number,
+            Response,
+            Skip
+        }
+        public UniqueList(Socket socket, Socket respond)
+        {
+            MessageType messageType = MessageType.Response;
+
+
+
+
+            int LOOP = 100;
+
+            write = new Thread(() =>
+            {
+                for (int i = 0; i < LOOP; i++)
+                {
+                    //lock (numbers)
+                    {
+                        int num = new Random().Next(100);
+                        if (!numbers.Contains(num))
+                        {
+                            //send number
+                            socket.Send(BitConverter.GetBytes(num));
+                            //read responce
+                            byte[] buffer = new byte[1024];
+                            var receive = respond.Receive(buffer);
+                            bool response = BitConverter.ToBoolean(buffer);
+
+                            if (response && !numbers.Contains(num))
+                            {
+                                Console.WriteLine($"{num}");
+                            }
+                            else
+                                Console.WriteLine($"cannot add(other)");
+
+                        }
+                        else
+                        {
+                            Console.WriteLine($"cannot add");
+                        }
+                    }
+                }
+            });
+
+            read = new Thread(() =>
+            {
+                for (int i = 0; i < LOOP; i++)
+                {
+                    //read number
+                    bool contain;
+                    lock (numbers)
+                    {
+                        byte[] bytes = new byte[1024];
+                        var responce = socket.Receive(bytes);
+
+                        int number = BitConverter.ToInt32(bytes);
+                        if (numbers.Contains(number))
+                            contain = true;
+                        else
+                        {
+                            contain = false;
+                            numbers.Add(number);
+                            Console.WriteLine($" {number}");
+                        }
+                    }
+                    //send responce
+                    respond.Send(BitConverter.GetBytes(contain));
+                }
+            });
+
+            read.Start();
+            write.Start();
+
+            read.Join();
+            write.Join();
+
+        }
+
+    }
+}
